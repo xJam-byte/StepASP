46e13da03cbc58eee92424746fd48a4c462e8199
xJam-byte <abylaimuratbayev@gmail.com>
2023-07-10 19:31:13 +0600

    Добавьте файлы проекта.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+  public class HomeController : Controller
+  {
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+      _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult Privacy()
+    {
+      return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    [HttpGet]
+    public async Task GetUsers() {
+      string content = @"
+          <form method = 'post'>
+          <label>User name: </label><br/>          
+          <input name='username' /><br/>
+          <label>Age: </label><br/>  
+          <input name='age' type='number'/> 
+          <input type='submit' value='Send'/>"; 
+    
+      Response.ContentType = "text/html;charset=utf-8";
+      await Response.WriteAsync(content);
+    }
+    
+    [HttpPost]
+    public string PostUsers(string username, int age) {
+      return $"{username}:{age}";      
+    }
+
+  }
+}
